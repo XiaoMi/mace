61f2e9d5e13d7f8fc7b19b4de3d1eb059cb2f54d
Liangliang He <lliang.he@gmail.com>
2017-12-05 18:00:42 +0800

    Change resize bilinear output size from tensor to attributes

--- mace/ops/resize_bilinear.h
+++ mace/ops/resize_bilinear.h
@@ -21,15 +21,12 @@ class ResizeBilinearOp : public Operator<D, T> {
 
   bool Run() override {
     const Tensor *input = this->Input(0);
-    const Tensor *resize_dims = this->Input(1);
     Tensor *output = this->Output(0);
 
     MACE_CHECK(input->dim_size() == 4, "input must be 4-dimensional.",
                input->dim_size());
-    MACE_CHECK(resize_dims->dim_size() == 1,
-               "resize dim must be 2-dimensional.", resize_dims->dim_size());
 
-    functor_(input, resize_dims, output);
+    functor_(input, output);
     return true;
   }
 
