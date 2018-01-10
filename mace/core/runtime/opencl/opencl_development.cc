96f486a57f77a2551cdc7bb32ae21720732ff433
Liangliang He <lliang.he@gmail.com>
2018-01-10 13:59:01 +0800

    Refactor tuning code as dev/prod library

--- mace/core/runtime/opencl/opencl_development.cc
+++ mace/core/runtime/opencl/opencl_development.cc
@@ -7,7 +7,6 @@
 #include "mace/core/runtime/opencl/cl2_header.h"
 #include "mace/utils/utils.h"
 
-
 namespace mace {
 
 bool GetSourceOrBinaryProgram(const std::string &program_name,
@@ -27,36 +26,6 @@ bool GetSourceOrBinaryProgram(const std::string &program_name,
   sources.push_back(ObfuscateString(kernel_source));
   *program = cl::Program(context, sources);
 
-  return true;
-}
-
-bool GetTuningParams(const char *path,
-                     std::unordered_map<std::string, std::vector<unsigned int>> *param_table) {
-  if (path != nullptr) {
-    std::ifstream ifs(path, std::ios::binary | std::ios::in);
-    if (ifs.is_open()) {
-      int64_t num_params = 0;
-      ifs.read(reinterpret_cast<char *>(&num_params), sizeof(num_params));
-      while (num_params--) {
-        int32_t key_size = 0;
-        ifs.read(reinterpret_cast<char *>(&key_size), sizeof(key_size));
-        std::string key(key_size, ' ');
-        ifs.read(&key[0], key_size);
-
-        int32_t params_size = 0;
-        ifs.read(reinterpret_cast<char *>(&params_size), sizeof(params_size));
-        int32_t params_count = params_size / sizeof(unsigned int);
-        std::vector<unsigned int> params(params_count);
-        for (int i = 0; i < params_count; ++i) {
-          ifs.read(reinterpret_cast<char *>(&params[i]), sizeof(unsigned int));
-        }
-        param_table->emplace(key, params);
-      }
-      ifs.close();
-    } else {
-      return false;
-    }
-  }
   return true;
 }
 
---
+++ mace/utils/tuner_production.cc
@@ -0,0 +1,21 @@
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace mace {
+
+bool GetTuningParams(
+    const char *path,
+    std::unordered_map<std::string, std::vector<unsigned int>> *param_table) {
+  extern const std::map<std::string, std::vector<unsigned int>>
+      kTuningParamsData;
+  for (auto it = kTuningParamsData.begin(); it != kTuningParamsData.end();
+       ++it) {
+    param_table->emplace(it->first, std::vector<unsigned int>(
+                                        it->second.begin(), it->second.end()));
+  }
+  return true;
+}
+
+}  // namespace mace
