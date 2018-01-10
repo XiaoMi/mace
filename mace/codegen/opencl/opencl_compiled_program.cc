e3a8c484cded3af3a937b15638f55515bbef78c2
yejianwu <yejianwu@xiaomi.com>
2018-01-10 16:44:45 +0800

    add blank vector into codegen

---
+++ mace/codegen/opencl/opencl_compiled_program.cc
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2017 XiaoMi All rights reserved.
+//
+
+// This is a generated file, DO NOT EDIT
+
+#include <map>
+#include <string>
+#include <vector>
+
+namespace mace {
+
+extern const std::map<std::string, std::vector<unsigned char>>
+    kCompiledProgramMap = {};
+
+}  // namespace
