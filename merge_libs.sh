8314dd64762568902c5b4071b1bb9c4df7486135
Liangliang He <lliang.he@gmail.com>
2018-02-26 15:49:24 +0800

    Merge all libs into one and update MaceEngine::Run interface

--- merge_libs.sh
+++ merge_libs.sh
@@ -19,36 +19,30 @@ MODEL_OUTPUT_DIRS_ARR=(${MODEL_OUTPUT_DIRS//,/ })
 rm -rf ${LIBMACE_BUILD_DIR}/libmace
 mkdir -p ${LIBMACE_BUILD_DIR}/libmace/lib
 cp -rf ${LIBMACE_SOURCE_DIR}/include ${LIBMACE_BUILD_DIR}/libmace/
-for model_output_dir in ${MODEL_OUTPUT_DIRS_ARR[@]}
-do
-  cp ${model_output_dir}/*.a ${LIBMACE_BUILD_DIR}/libmace/lib/
-done
 cp ${LIBMACE_SOURCE_DIR}/lib/hexagon/libhexagon_controller.so ${LIBMACE_BUILD_DIR}/libmace/lib
 
 LIBMACE_TEMP_DIR=`mktemp -d -t libmace.XXXX`
 
-# Merge libmace engine
-echo "create ${LIBMACE_BUILD_DIR}/libmace/lib/libmace_engine.a" > ${LIBMACE_TEMP_DIR}/libmace_engine.mri
-echo "addlib lib/mace/libmace.a" >> ${LIBMACE_TEMP_DIR}/libmace_engine.mri
-echo "addlib lib/mace/libmace_prod.a" >> ${LIBMACE_TEMP_DIR}/libmace_engine.mri
-echo "save" >> ${LIBMACE_TEMP_DIR}/libmace_engine.mri
-echo "end" >> ${LIBMACE_TEMP_DIR}/libmace_engine.mri
-$ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar \
-    -M < ${LIBMACE_TEMP_DIR}/libmace_engine.mri || exit 1
-
-# Merge opencl bin and tuning result
-echo "create ${LIBMACE_BUILD_DIR}/libmace/lib/libmace_opencl.a" > ${LIBMACE_TEMP_DIR}/libmace_opencl.mri
+# Merge all libraries in to one
+echo "create ${LIBMACE_BUILD_DIR}/libmace/lib/libmace_with_models.a" > ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
+echo "addlib lib/mace/libmace.a" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
+echo "addlib lib/mace/libmace_prod.a" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
 if [ x"TARGET_ABI" = x"host" ]; then
-  echo "addlib bazel-bin/codegen/libgenerated_opencl_prod.pic.a" >> ${LIBMACE_TEMP_DIR}/libmace_opencl.mri
-  echo "addlib bazel-bin/codegen/libgenerated_tuning_params.pic.a" >> ${LIBMACE_TEMP_DIR}/libmace_opencl.mri
+  echo "addlib bazel-bin/codegen/libgenerated_opencl_prod.pic.a" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
+  echo "addlib bazel-bin/codegen/libgenerated_tuning_params.pic.a" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
 else
-  echo "addlib bazel-bin/codegen/libgenerated_opencl_prod.a" >> ${LIBMACE_TEMP_DIR}/libmace_opencl.mri
-  echo "addlib bazel-bin/codegen/libgenerated_tuning_params.a" >> ${LIBMACE_TEMP_DIR}/libmace_opencl.mri
+  echo "addlib bazel-bin/codegen/libgenerated_opencl_prod.a" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
+  echo "addlib bazel-bin/codegen/libgenerated_tuning_params.a" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
 fi
-echo "save" >> ${LIBMACE_TEMP_DIR}/libmace_opencl.mri
-echo "end" >> ${LIBMACE_TEMP_DIR}/libmace_opencl.mri
+for model_output_dir in ${MODEL_OUTPUT_DIRS_ARR[@]}; do
+  for lib in ${model_output_dir}/*.a; do
+    echo "addlib ${lib}" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
+  done
+done
+echo "save" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
+echo "end" >> ${LIBMACE_TEMP_DIR}/libmace_with_models.mri
 $ANDROID_NDK_HOME/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar \
-    -M < ${LIBMACE_TEMP_DIR}/libmace_opencl.mri || exit 1
+    -M < ${LIBMACE_TEMP_DIR}/libmace_with_models.mri || exit 1
 
 rm -rf ${LIBMACE_TEMP_DIR}
 
