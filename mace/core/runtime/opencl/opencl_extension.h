776748e3be66f635f7c610840b6a5627e63ae468
Liangliang He <lliang.he@gmail.com>
2018-03-08 11:03:01 +0800

    Add Adreno performance hints

---
+++ mace/core/runtime/opencl/opencl_extension.h
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2017 XiaoMi All rights reserved.
+//
+
+#ifndef MACE_CORE_RUNTIME_OPENCL_EXTENSION_H_
+#define MACE_CORE_RUNTIME_OPENCL_EXTENSION_H_
+
+#include "mace/core/runtime/opencl/cl2_header.h"
+
+// Adreno extensions
+// Adreno performance hints
+typedef cl_uint cl_perf_hint;
+
+#define CL_CONTEXT_PERF_HINT_QCOM 0x40C2
+#define CL_PERF_HINT_HIGH_QCOM 0x40C3
+#define CL_PERF_HINT_NORMAL_QCOM 0x40C4
+#define CL_PERF_HINT_LOW_QCOM 0x40C5
+
+// Adreno priority hints
+typedef cl_uint cl_priority_hint;
+
+#define CL_PRIORITY_HINT_NONE_QCOM 0
+#define CL_CONTEXT_PRIORITY_HINT_QCOM 0x40C9
+#define CL_PRIORITY_HINT_HIGH_QCOM 0x40CA
+#define CL_PRIORITY_HINT_NORMAL_QCOM 0x40CB
+#define CL_PRIORITY_HINT_LOW_QCOM 0x40CC
+
+#endif  // MACE_CORE_RUNTIME_OPENCL_EXTENSION_H_
