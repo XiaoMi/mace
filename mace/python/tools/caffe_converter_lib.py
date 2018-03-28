a1c6ba9257c0b8abea09959f0d6f832a13b526c5
liuqi <liuqi10@xiaomi.com>
2018-03-28 14:06:30 +0800

    Fix input_files config bug.

--- mace/python/tools/caffe_converter_lib.py
+++ mace/python/tools/caffe_converter_lib.py
@@ -789,7 +789,6 @@ class CaffeConverter(object):
     input_shape = op.parents[0].output_shape_map[op.layer.bottom[0]]
     output_shape = input_shape
     shape_param = np.asarray(op.layer.reshape_param.shape.dim)[[0, 3, 2, 1]]
-    print shape_param
     for i in range(len(shape_param)):
       if shape_param[i] != 0:
         output_shape[i] = shape_param[i]
