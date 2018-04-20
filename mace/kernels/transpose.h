1dcd83b134cc83a0fcf0522cc1ede8f05c39d783
李寅 <liyinhgqw@gmail.com>
2018-04-20 14:32:15 +0800

    Add transpose 2D

--- mace/kernels/transpose.h
+++ mace/kernels/transpose.h
@@ -37,31 +37,44 @@ struct TransposeFunctor {
     const T *input_data = input->data<T>();
     T *output_data = output->mutable_data<T>();
 
-    std::vector<index_t>
-      in_stride{input_shape[1] * input_shape[2] * input_shape[3],
-                input_shape[2] * input_shape[3], input_shape[3], 1};
-    std::vector<index_t>
-      out_stride{output_shape[1] * output_shape[2] * output_shape[3],
-                 output_shape[2] * output_shape[3], output_shape[3], 1};
+    if (input->dim_size() == 2) {
+      MACE_CHECK(dims_[0] == 1 && dims_[1] == 0, "no need transform");
+      index_t stride_i = input_shape[0];
+      index_t stride_j = input_shape[1];
+      for (int i = 0; i < input_shape[0]; ++i) {
+        for (int j = 0; j < input_shape[1]; ++j) {
+          output_data[j * stride_i + i] = input_data[i * stride_j + j];
+        }
+      }
+    } else if (input->dim_size() == 4) {
+      std::vector<index_t>
+        in_stride{input_shape[1] * input_shape[2] * input_shape[3],
+                  input_shape[2] * input_shape[3], input_shape[3], 1};
+      std::vector<index_t>
+        out_stride{output_shape[1] * output_shape[2] * output_shape[3],
+                   output_shape[2] * output_shape[3], output_shape[3], 1};
 
-    std::vector<index_t> idim(4, 0);
-    std::vector<index_t> odim(4, 0);
-    for (odim[0] = 0; odim[0] < output_shape[0]; ++odim[0]) {
-      for (odim[1] = 0; odim[1] < output_shape[1]; ++odim[1]) {
-        for (odim[2] = 0; odim[2] < output_shape[2]; ++odim[2]) {
-          for (odim[3] = 0; odim[3] < output_shape[3]; ++odim[3]) {
-            idim[dims_[0]] = odim[0];
-            idim[dims_[1]] = odim[1];
-            idim[dims_[2]] = odim[2];
-            idim[dims_[3]] = odim[3];
+      std::vector<index_t> idim(4, 0);
+      std::vector<index_t> odim(4, 0);
+      for (odim[0] = 0; odim[0] < output_shape[0]; ++odim[0]) {
+        for (odim[1] = 0; odim[1] < output_shape[1]; ++odim[1]) {
+          for (odim[2] = 0; odim[2] < output_shape[2]; ++odim[2]) {
+            for (odim[3] = 0; odim[3] < output_shape[3]; ++odim[3]) {
+              idim[dims_[0]] = odim[0];
+              idim[dims_[1]] = odim[1];
+              idim[dims_[2]] = odim[2];
+              idim[dims_[3]] = odim[3];
 
-            output_data[odim[0] * out_stride[0] + odim[1] * out_stride[1]
-              + odim[2] * out_stride[2] + odim[3]] =
-              input_data[idim[0] * in_stride[0] + idim[1] * in_stride[1]
-                + idim[2] * in_stride[2] + idim[3]];
+              output_data[odim[0] * out_stride[0] + odim[1] * out_stride[1]
+                + odim[2] * out_stride[2] + odim[3]] =
+                input_data[idim[0] * in_stride[0] + idim[1] * in_stride[1]
+                  + idim[2] * in_stride[2] + idim[3]];
+            }
           }
         }
       }
+    } else {
+      MACE_NOT_IMPLEMENTED;
     }
   }
 
