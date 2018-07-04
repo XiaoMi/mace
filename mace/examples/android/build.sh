32f4f54d7af3cf040e30e5e2bb7153898c75e5a1
Liangliang He <lliang.he@gmail.com>
2018-07-04 08:54:55 +0800

    Fix Android demo model deployment file link

--- mace/examples/android/build.sh
+++ mace/examples/android/build.sh
@@ -3,7 +3,7 @@
 set -e -u -o pipefail
 
 pushd ../../../
-python tools/converter.py build --config=docs/getting_started/models/demo_app_models.yaml
+python tools/converter.py build --config=docs/user_guide/models/demo_app_models.yml
 
 cp -r builds/mobilenet/include/ mace/examples/android/macelibrary/src/main/cpp/
 cp -r builds/mobilenet/lib/ mace/examples/android/macelibrary/src/main/cpp/
