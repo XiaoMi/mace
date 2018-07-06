185755c8e03729fd8a7d926146dc3aa9bf761a60
李寅 <liyin@xiaomi.com>
2018-07-06 14:07:56 +0800

    Differentiate libs w/ and w/o hexagon dsp

--- tools/build-standalone-lib.sh
+++ tools/build-standalone-lib.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-LIB_DIR=build/lib
-INCLUDE_DIR=build/include/mace/public
+LIB_DIR=builds/lib
+INCLUDE_DIR=builds/include/mace/public
 
 mkdir -p $LIB_DIR
 mkdir -p $INCLUDE_DIR
@@ -12,25 +12,27 @@ mkdir -p $INCLUDE_DIR
 cp mace/public/*.h $INCLUDE_DIR/
 
 echo "build lib for armeabi-v7a"
+rm -rf $LIB_DIR/armeabi-v7a
 mkdir -p $LIB_DIR/armeabi-v7a
-rm -f $LIB_DIR/armeabi-v7a/*
-bazel build --config android mace:libmace --define neon=true --define openmp=true --define hexagon=true --cpu=armeabi-v7a
+bazel build --config android --config optimization mace:libmace --cpu=armeabi-v7a --define neon=true --define openmp=true
 cp bazel-bin/mace/libmace.so $LIB_DIR/armeabi-v7a/
-cp third_party/nnlib/*so $LIB_DIR/armeabi-v7a/
+
+echo "build lib for armeabi-v7a with hexagon dsp"
+rm -rf $LIB_DIR/armeabi-v7a/hexagon-dsp
+mkdir -p $LIB_DIR/armeabi-v7a/hexagon-dsp
+bazel build --config android --config optimization mace:libmace --cpu=armeabi-v7a --define neon=true --define openmp=true --define hexagon=true
+cp bazel-bin/mace/libmace.so $LIB_DIR/armeabi-v7a/hexagon-dsp/
+cp third_party/nnlib/*so $LIB_DIR/armeabi-v7a/hexagon-dsp/
 
 echo "build lib for arm64-v8a"
+rm -rf $LIB_DIR/arm64-v8a
 mkdir -p $LIB_DIR/arm64-v8a
-rm -f $LIB_DIR/arm64-v8a/*
-bazel build --config android mace:libmace --define neon=true --define openmp=true --cpu=arm64-v8a
+bazel build --config android --config optimization mace:libmace --cpu=arm64-v8a --define neon=true --define openmp=true
 cp bazel-bin/mace/libmace.so $LIB_DIR/arm64-v8a/
 
 echo "build lib for linux-x86-64"
+rm -rf $LIB_DIR/linux-x86-64
 mkdir -p $LIB_DIR/linux-x86-64
-rm -f $LIB_DIR/linux-x86-64/*
-bazel build mace:libmace --define openmp=true
+bazel build --config optimization mace:libmace --define openmp=true
 cp bazel-bin/mace/libmace.so $LIB_DIR/linux-x86-64/
-
-
-
-
 
