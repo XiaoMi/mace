54f86233ea7659ef20d2b3ac53e11cec3078fafc
byronhsu <byronhsu1230@gmail.com>
2018-07-18 15:08:18 +0800

    neglect linux building on mac temporarily

--- tools/build-standalone-lib.sh
+++ tools/build-standalone-lib.sh
@@ -31,10 +31,11 @@ echo "build shared lib for arm64-v8a"
 bazel build --config android --config optimization mace/libmace:libmace_dynamic --define neon=true --define openmp=true --define opencl=true --cpu=arm64-v8a
 cp bazel-bin/mace/libmace/libmace.so $LIB_DIR/arm64-v8a/
 
-echo "build shared lib for linux-x86-64"
-bazel build mace/libmace:libmace_dynamic --config optimization --define openmp=true
-cp bazel-bin/mace/libmace/libmace.so $LIB_DIR/linux-x86-64/
-
+if [[ "$OSTYPE" != "darwin"* ]];then
+	echo "build shared lib for linux-x86-64"
+	bazel build mace/libmace:libmace_dynamic --config optimization --define openmp=true
+	cp bazel-bin/mace/libmace/libmace.so $LIB_DIR/linux-x86-64/
+fi
 
 # build static libraries
 echo "build static lib for armeabi-v7a"
@@ -46,9 +47,11 @@ echo "build static lib for arm64-v8a"
 bazel build --config android --config optimization mace/libmace:libmace_static --define neon=true --define openmp=true --define opencl=true --cpu=arm64-v8a
 cp bazel-genfiles/mace/libmace/libmace.a $LIB_DIR/arm64-v8a/
 
-echo "build static lib for linux-x86-64"
-bazel build mace/libmace:libmace_static --config optimization --define openmp=true
-cp bazel-genfiles/mace/libmace/libmace.a $LIB_DIR/linux-x86-64/
+if [[ "$OSTYPE" != "darwin"* ]];then
+	echo "build static lib for linux-x86-64"
+	bazel build mace/libmace:libmace_static --config optimization --define openmp=true
+	cp bazel-genfiles/mace/libmace/libmace.a $LIB_DIR/linux-x86-64/
+fi
 
 echo "LIB PATH: $LIB_DIR"
 echo "INCLUDE FILE PATH: $INCLUDE_DIR"
