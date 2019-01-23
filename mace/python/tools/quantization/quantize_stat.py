a0c1f222294c0dd1b41f9e3ddc54d33b9f55e68c
李寅 <liyin@xiaomi.com>
2019-01-23 14:13:50 +0800

    "Quantization enhancement: Bring gain of top-1 accuracy (69.54% -> 70.20%)

--- mace/python/tools/quantization/quantize_stat.py
+++ mace/python/tools/quantization/quantize_stat.py
@@ -7,7 +7,7 @@ class QuantizeStat(object):
         pass
 
     @staticmethod
-    def run(log_file, percentile):
+    def run(log_file, percentile, enhance, enhance_ratio):
         res = {}
         tensor_ranges = {}
         with open(log_file) as log:
@@ -22,12 +22,41 @@ class QuantizeStat(object):
                     tensor_ranges[tensor_name][1].append(max_val)
 
         for tensor_name in tensor_ranges:
+            samples = len(tensor_ranges[tensor_name][0])
             tensor_min = np.percentile(tensor_ranges[tensor_name][0],
                                        percentile)
             tensor_max = np.percentile(tensor_ranges[tensor_name][1],
                                        100 - percentile)
             assert tensor_min < tensor_max
-            res[tensor_name] = (tensor_min, tensor_max)
+            if not enhance or samples <= 1:
+                res[tensor_name] = (tensor_min, tensor_max)
+            else:
+                tensor_mins = np.sort(tensor_ranges[tensor_name][0])
+                tensor_maxs = np.sort(tensor_ranges[tensor_name][1])[::-1]
+                cur_min_idx = 0
+                cur_max_idx = 0
+                cur_min = tensor_min
+                cur_max = tensor_max
+                for i in xrange(samples):
+                    if tensor_mins[i] + 0.1 > cur_max:
+                        break
+
+                    delta_range = (tensor_mins[i] - cur_min) / (cur_max - cur_min)  # noqa
+                    delta_quantile = float(i - cur_min_idx) / (samples - cur_min_idx)  # noqa
+                    if delta_quantile > 0 and delta_range / delta_quantile > enhance_ratio:  # noqa
+                        cur_min_idx = i
+                        cur_min = tensor_mins[i]
+
+                    if cur_min + 0.1 > tensor_maxs[i]:
+                        break
+
+                    delta_range = (cur_max - tensor_maxs[i]) / (cur_max - cur_min)  # noqa
+                    delta_quantile = float(i - cur_max_idx) / (samples - cur_max_idx)  # noqa
+                    if delta_quantile > 0 and delta_range / delta_quantile > enhance_ratio:  # noqa
+                        cur_max_idx = i
+                        cur_max = tensor_maxs[i]
+
+                res[tensor_name] = (cur_min, cur_max)
 
         return res
 
@@ -42,10 +71,20 @@ if __name__ == '__main__':
     parser.add_argument(
         "--percentile",
         type=int,
-        default=5,
+        default=0,
         help="range percentile")
+    parser.add_argument(
+        "--enhance",
+        action="store_true",
+        help="range percentile")
+    parser.add_argument(
+        "--enhance_ratio",
+        type=int,
+        default=10,
+        help="enhance ratio")
     FLAGS, unparsed = parser.parse_known_args()
 
-    res = QuantizeStat.run(FLAGS.log_file, FLAGS.percentile)
+    res = QuantizeStat.run(FLAGS.log_file, FLAGS.percentile, FLAGS.enhance,
+                           FLAGS.enhance_ratio)
     for tensor in res:
         print("%s@@%f,%f" % (tensor, res[tensor][0], res[tensor][1]))
