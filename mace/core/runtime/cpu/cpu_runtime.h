5371f9864b11f46944de4f3c17f4636daa754e83
liuqi <liuqi10@xiaomi.com>
2019-03-25 16:00:45 +0800

    BUG: memory leak gemm_context_ object(2097840 bytes) in cpu_runtime.h

--- mace/core/runtime/cpu/cpu_runtime.h
+++ mace/core/runtime/cpu/cpu_runtime.h
@@ -52,13 +52,13 @@ class CPURuntime {
 
 #ifdef MACE_ENABLE_QUANTIZE
   ~CPURuntime() {
-    if (!gemm_context_) {
+    if (gemm_context_ != nullptr) {
       delete static_cast<gemmlowp::GemmContext*>(gemm_context_);
     }
   }
 
   gemmlowp::GemmContext *GetGemmlowpContext() {
-    if (!gemm_context_) {
+    if (gemm_context_ == nullptr) {
       gemm_context_ = new gemmlowp::GemmContext();
     }
     return static_cast<gemmlowp::GemmContext*>(gemm_context_);
