384099e58214bcebb74f529bbba936917659bf45
Liangliang He <lliang.he@gmail.com>
2019-05-08 12:17:36 +0800

    Fix cpplint

--- tools/cpplint.sh
+++ tools/cpplint.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-curl -o cpplint.py https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
-python cpplint.py --linelength=80 --counting=detailed $(find mace -name "*.h" -or -name "*.cc")
-python cpplint.py --linelength=80 --counting=detailed --root=include $(find include -name "*.h" -or -name "*.cc")
-python cpplint.py --linelength=80 --counting=detailed --root=test/ccutils $(find test/ccutils -name "*.h" -or -name "*.cc")
-python cpplint.py --linelength=80 --counting=detailed --root=test/ccunit $(find test/ccunit -name "*.h" -or -name "*.cc")
-python cpplint.py --linelength=80 --counting=detailed --root=test/ccbenchmark $(find test/ccbenchmark -name "*.h" -or -name "*.cc")
-rm cpplint.py
+cpplint --linelength=80 --counting=detailed $(find mace -name "*.h" -or -name "*.cc")
+cpplint --linelength=80 --counting=detailed --root=include $(find include -name "*.h" -or -name "*.cc")
+cpplint --linelength=80 --counting=detailed --root=test/ccutils $(find test/ccutils -name "*.h" -or -name "*.cc")
+cpplint --linelength=80 --counting=detailed --root=test/ccunit $(find test/ccunit -name "*.h" -or -name "*.cc")
+cpplint --linelength=80 --counting=detailed --root=test/ccbenchmark $(find test/ccbenchmark -name "*.h" -or -name "*.cc")
