46bf5a0e7924594f2258caada713e035a3c6226d
like15 <like15@xiaomi.com>
2021-01-22 17:08:47 +0800

    feat: Support split tensor unequally by second input of Split Op

--- mace/ops/split.cc
+++ mace/ops/split.cc
@@ -46,8 +46,31 @@ class SplitOp<DeviceType::CPU, T> : public Operation {
     }
     MACE_CHECK(this->OutputSize() >= 2)
       << "There must be at least two outputs for slicing";
-    MACE_CHECK((this->Input(0)->dim(axis_) % this->OutputSize()) == 0)
-      << "Outputs do not split input equally.";
+    const Tensor *split_tensor =
+        this->InputSize() == 2 ? this->Input(1) : nullptr;
+    if ((this->Input(0)->dim(axis_) % this->OutputSize())) {
+      MACE_CHECK(split_tensor != nullptr)
+        << "To split input unequally, split sizes must be specified.";
+    }
+    if (split_tensor != nullptr) {
+      index_t split_dim_size = split_tensor->dim_size();
+      MACE_CHECK(split_dim_size == 1) << "Split input data must be 1D tensor, "
+          << split_dim_size << "D data is got.";
+      index_t num_splits = split_tensor->dim(0);
+      MACE_CHECK(num_splits == this->OutputSize())
+          << "Output size (" << this->OutputSize()
+          << ") must be equal to split size ("
+          << num_splits << ").";
+      DataType split_dt = split_tensor->dtype();
+      MACE_CHECK(split_dt == DataType::DT_INT32)
+          << "Split tensor must have int32 datatype, "
+          << "datatype " << split_dt << " is got.";
+    } else {
+      MACE_CHECK((this->Input(0)->dim(axis_) % this->OutputSize()) == 0)
+        << "When split sizes are not specified, "
+        << "outputs must split input equally.";
+    }
+
     checked_ = true;
   }
 
@@ -55,23 +78,43 @@ class SplitOp<DeviceType::CPU, T> : public Operation {
     MACE_UNUSED(context);
     if (!checked_) Validate();
     const Tensor *input = this->Input(0);
+    Tensor::MappingGuard input_guard(input);
+    const Tensor *split_tensor =
+        this->InputSize() == 2 ? this->Input(1) : nullptr;
     const std::vector<Tensor *> output_list = this->Outputs();
-    const index_t input_channels = input->dim(axis_);
     const size_t outputs_count = output_list.size();
-    const index_t output_channels = input_channels / outputs_count;
+    const index_t input_channels = input->dim(axis_);
+    std::vector<index_t> output_channels_list = std::vector<index_t>(
+          outputs_count, input_channels / outputs_count);
+    std::vector<std::vector<index_t>> output_shape_list;
+    const std::vector<index_t> &input_shape = input->shape();
+    if (split_tensor != nullptr) {
+      Tensor::MappingGuard split_guard(split_tensor);
+      const int32_t *split_data = split_tensor->data<int32_t>();
+      output_shape_list = std::vector<std::vector<index_t>>(
+          outputs_count, input_shape);
+      for (size_t i = 0; i < outputs_count; ++i) {
+        output_channels_list[i] = static_cast<index_t>(split_data[i]);
+        output_shape_list[i][axis_] = output_channels_list[i];
+      }
+    } else {
+      std::vector<index_t> output_shape(input->shape());
+      output_shape[axis_] = output_channels_list[0];
+      output_shape_list = std::vector<std::vector<index_t>>(
+          outputs_count, output_shape);
+    }
     std::vector<T *> output_ptrs(output_list.size(), nullptr);
-    std::vector<index_t> output_shape(input->shape());
-    output_shape[axis_] = output_channels;
 
-    const index_t outer_size = std::accumulate(output_shape.begin(),
-                                               output_shape.begin() + axis_,
+    const index_t outer_size = std::accumulate(input_shape.begin(),
+                                               input_shape.begin() + axis_,
                                                1,
                                                std::multiplies<index_t>());
-    const index_t inner_size = std::accumulate(output_shape.begin() + axis_ + 1,
-                                               output_shape.end(),
+    const index_t inner_size = std::accumulate(input_shape.begin() + axis_ + 1,
+                                               input_shape.end(),
                                                1,
                                                std::multiplies<index_t>());
     for (size_t i = 0; i < outputs_count; ++i) {
+      const std::vector<index_t> &output_shape = output_shape_list[i];
       MACE_RETURN_IF_ERROR(output_list[i]->Resize(output_shape));
       output_ptrs[i] = output_list[i]->mutable_data<T>();
     }
@@ -79,17 +122,18 @@ class SplitOp<DeviceType::CPU, T> : public Operation {
 
     for (int outer_idx = 0; outer_idx < outer_size; ++outer_idx) {
       index_t input_idx = outer_idx * input_channels * inner_size;
-      index_t output_idx = outer_idx * output_channels * inner_size;
+      index_t multiplier = outer_idx * inner_size;
       for (size_t i = 0; i < outputs_count; ++i) {
+        index_t output_idx = multiplier * output_channels_list[i];
         if (DataTypeCanUseMemcpy(DataTypeToEnum<T>::v())) {
           memcpy(output_ptrs[i] + output_idx, input_ptr + input_idx,
-                 output_channels * inner_size * sizeof(T));
+                 output_channels_list[i] * inner_size * sizeof(T));
         } else {
-          for (index_t k = 0; k < output_channels * inner_size; ++k) {
+          for (index_t k = 0; k < output_channels_list[i] * inner_size; ++k) {
             *(output_ptrs[i] + output_idx + k) = *(input_ptr + input_idx + k);
           }
         }
-        input_idx += output_channels * inner_size;
+        input_idx += output_channels_list[i] * inner_size;
       }
     }
     return MaceStatus::MACE_SUCCESS;
@@ -147,7 +191,8 @@ void RegisterSplit(OpRegistry *op_registry) {
                 int axis = ProtoArgHelper::GetOptionalArg<OperatorDef, int>(
                     *op, "axis", 3);
                 if (axis != 3 || op->output_shape(0).dims_size() != 4 ||
-                    (op->output_shape(0).dims()[3] % 4 != 0)) {
+                    (op->output_shape(0).dims()[3] % 4 != 0) ||
+                    op->input_size() > 1) {
                   return {DeviceType::CPU};
                 }
                 return {DeviceType::CPU, DeviceType::GPU};
