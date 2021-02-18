7997a0d78ae6b6e28a37d3234db056889630d073
Riki <rgall@sertiscorp.com>
2021-02-18 10:03:26 +0700

    Fix build for macOS and Windows, support rpcmem

--- third_party/rpcmem/rpcmem.cmake
+++ third_party/rpcmem/rpcmem.cmake
@@ -6,5 +6,3 @@ include_directories(SYSTEM "${RPCMEM_INCLUDE_DIR}")
 set(RPCMEM_LIB "${RPCMEM_INSTALL_DIR}/${ANDROID_ABI}/rpcmem.a")
 add_library(rpcmem STATIC IMPORTED GLOBAL)
 set_target_properties(rpcmem PROPERTIES IMPORTED_LOCATION ${RPCMEM_LIB})
-
-install(FILES ${RPCMEM_LIB} DESTINATION lib)
